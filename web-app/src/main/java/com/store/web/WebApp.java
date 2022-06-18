ab4903fbdb4d67461ef1994d09fd7ebefa2b69d8
orn974@gmail.com <Grigorik>
2022-06-18 11:07:48 +0300

    v 0.6.1 + war + for run with tomcat

--- web-app/src/main/java/com/store/web/WebApp.java
+++ web-app/src/main/java/com/store/web/WebApp.java
@@ -3,10 +3,11 @@ package com.store.web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @EntityScan("com.store.model")
 @SpringBootApplication
-public class WebApp {
+public class WebApp extends SpringBootServletInitializer {
     public static void main( String[] args )
     {
         SpringApplication.run(WebApp.class, args);
