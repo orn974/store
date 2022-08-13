aaffd9da7a4874f88bb8028bf3d6792a750fbb57
orn974@gmail.com <Grigorik>
2022-08-13 12:08:25 +0300

    v 0.6.1 + war + for run with tomcat + test begin

---
+++ dao/src/test/java/com/store/dao/TestStoreRepository.java
@@ -0,0 +1,4 @@
+package com.store.dao;
+
+public class TestStoreRepository {
+}
--- model/src/test/java/com/store/model/ProductTest.java
+++ model/src/test/java/com/store/model/ProductTest.java
@@ -2,57 +2,58 @@ package com.store.model;
 
 
 import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class ProductTest {
 
     private Product product = new Product();
+
     @BeforeAll  // start before all test
-    @Disabled // put off test
-    public void allSetId (){
-        product.setStoreId(44L);
+    //@Disabled // put off test
+    public static void allSetId (){
+        System.out.println("BeforeAll");
+       // product.setStoreId(44L);
     }
 
     @BeforeEach // start before each test
     public void setEachStoreId (){
         product.setStoreId(110L);
-        System.out.println(product.getStoreId()+"DDDDDDDDDDD");
+        System.out.println(product.getStoreId()+"  BeforeEach");
     }
 
 
     @Test
     public void getStoreIdTest(){
         product.setStoreId(9L);
-       //assertTrue(product.getStoreId().equals(9L));
-        Assertions.assertTrue(product.getStoreId().equals(9L));
+        assertTrue(product.getStoreId().equals(9L));
     }
 
     @Test
     public void limitStoreIdTest (){
         product.setStoreId(-20L);
-        Assertions.assertTrue(product.getStoreId().equals(0L));
+        assertTrue(product.getStoreId().equals(0L));
 
     }
 
     @Test
     public void limitMaxTStoreIdTest(){
         product.setStoreId(101L);
-        Assertions.assertTrue(product.getStoreId().equals(100L));
+        assertTrue(product.getStoreId().equals(100L));
     }
     @Test
     public void testNoteqalesStoreId (){
         product.setStoreId(60L);
-        Assertions.assertNotEquals(java.util.Optional.of(66L), product.getStoreId());
-        Assertions.assertEquals(Long.valueOf(60), product.getStoreId());
+        assertNotEquals(66L, product.getStoreId());
+        assertEquals(60L, product.getStoreId());
     }
 
     @Test
     public void failTest() {
-        product.setStoreId(100l);
+        product.setStoreId(1L);
         System.out.println(product.getStoreId());
         if (product.getStoreId() == 100L) {
-            Assertions.fail("StireId = 100");
+            fail("StireId = 100");
 
         }
     }
